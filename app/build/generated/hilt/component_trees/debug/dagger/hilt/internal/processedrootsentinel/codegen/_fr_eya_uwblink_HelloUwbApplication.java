4e073129b0bf309e52f5d6b8a4a46bddc15931e4
KhalilHammami <khammami@gmail.com>
2024-05-28 10:45:30 +0100

    drop build files

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_fr_eya_uwblink_HelloUwbApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "fr.eya.uwblink.HelloUwbApplication"
-)
-public final class _fr_eya_uwblink_HelloUwbApplication {
-}
